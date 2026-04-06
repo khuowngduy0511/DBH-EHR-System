84643de00c9113f4a8c6061355d9e9c78c87b8e8
khuowngduy <khuongduy.tech@gmail.com>
2026-04-06 08:15:04 +0700

    chore(ehr): add EF Core migration RefactorRemoveMongoAddIpfs

--- src/DBH.EHR.Service/Migrations/20260405233150_RefactorRemoveMongoAddIpfs.Designer.cs
+++ src/DBH.EHR.Service/Migrations/20260405233150_RefactorRemoveMongoAddIpfs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DBH.EHR.Service.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DBH.EHR.Service.Migrations
 {
     [DbContext(typeof(EhrPrimaryDbContext))]
-    partial class EhrPrimaryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260405233150_RefactorRemoveMongoAddIpfs")]
+    partial class RefactorRemoveMongoAddIpfs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DBH.EHR.Service/Migrations/20260405233150_RefactorRemoveMongoAddIpfs.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBH.EHR.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorRemoveMongoAddIpfs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "data",
+                table: "ehr_versions");
+
+            migrationBuilder.DropColumn(
+                name: "data",
+                table: "ehr_records");
+
+            migrationBuilder.AddColumn<string>(
+                name: "data_hash",
+                table: "ehr_versions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "encrypted_fallback_data",
+                table: "ehr_versions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ipfs_cid",
+                table: "ehr_versions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "encrypted_fallback_data",
+                table: "ehr_files",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ipfs_cid",
+                table: "ehr_files",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "data_hash",
+                table: "ehr_versions");
+
+            migrationBuilder.DropColumn(
+                name: "encrypted_fallback_data",
+                table: "ehr_versions");
+
+            migrationBuilder.DropColumn(
+                name: "ipfs_cid",
+                table: "ehr_versions");
+
+            migrationBuilder.DropColumn(
+                name: "encrypted_fallback_data",
+                table: "ehr_files");
+
+            migrationBuilder.DropColumn(
+                name: "ipfs_cid",
+                table: "ehr_files");
+
+            migrationBuilder.AddColumn<string>(
+                name: "data",
+                table: "ehr_versions",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "data",
+                table: "ehr_records",
+                type: "jsonb",
+                nullable: true);
+        }
+    }
+}
