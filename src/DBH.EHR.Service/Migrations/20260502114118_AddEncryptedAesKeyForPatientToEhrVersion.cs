10aa22a20902cae984f7d40c7db33264c7b08c50
khuowngduy <khuongduy.tech@gmail.com>
2026-05-02 21:11:06 +0700

    feat(ehr/db): add migration for EncryptedAesKeyForPatient on EhrVersion

---
+++ src/DBH.EHR.Service/Migrations/20260502114118_AddEncryptedAesKeyForPatientToEhrVersion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBH.EHR.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEncryptedAesKeyForPatientToEhrVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "encrypted_aes_key_for_patient",
+                table: "ehr_versions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "encrypted_aes_key_for_patient",
+                table: "ehr_versions");
+        }
+    }
+}
