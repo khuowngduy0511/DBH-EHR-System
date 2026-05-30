f14be0b2447a4437ff1ea8fe6b154f9b25c62863
socday <socpro13579@gmail.com>
2026-05-30 16:56:48 +0700

    Chore: clean up file

--- test-ca-register.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Test Fabric CA register with Basic Auth
-curl -ks -u admin:adminpw \
-  -X POST https://ca_hospital1:7054/api/v1/register \
-  -H "Content-Type: application/json" \
-  -d '{"id":"testuser999","type":"client","secret":"testpw123456","affiliation":"org1.department1","attrs":[{"name":"username","value":"test","ecert":true}],"caname":"ca-hospital1"}'
